199f83b57437ed65b110c6957e807f8875559d10
incisors <haj003@ucsd.edu>
2023-04-22 17:04:04 -0700

    header update

---
+++ movingIcon.js
@@ -0,0 +1,25 @@
+const movingIcon = document.getElementById('movingIcon');
+let xPosition = 0;
+let yPosition = 0;
+let xSpeed = 0.5;
+let ySpeed = 0.5;
+
+function moveIcon() {
+    xPosition += xSpeed;
+    yPosition += ySpeed;
+
+    if (xPosition + movingIcon.clientWidth > document.documentElement.clientWidth || xPosition < 0) {
+        xSpeed = -xSpeed;
+    }
+
+    if (yPosition + movingIcon.clientHeight > document.documentElement.clientHeight || yPosition < 0) {
+        ySpeed = -ySpeed;
+    }
+
+    movingIcon.style.left = xPosition + 'px';
+    movingIcon.style.top = yPosition + 'px';
+
+    requestAnimationFrame(moveIcon);
+}
+
+moveIcon();
